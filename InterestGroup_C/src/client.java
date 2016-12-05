229fa71d285be08b6cef93afb3df116c081f63bb
melanie0926 <melanieeelin@gmail.com>
2016-12-05 16:00:44 -0500

    setup connection

--- InterestGroup_C/src/client.java
+++ InterestGroup_C/src/client.java
@@ -41,6 +41,8 @@ public class client{
                     System.out.println(str);
                 }
             }
-        }catch (Exception e) {}
+        }catch (Exception e) {
+
+        }
     }
 }
--- InterestGroup_C/src/interest_group_c/InterestGroup_Client.java
+++ InterestGroup_C/src/interest_group_c/InterestGroup_Client.java
@@ -5,6 +5,13 @@ package interest_group_c;
  * @author majiasheng
  */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * client side 
  */
@@ -14,7 +21,43 @@ public class InterestGroup_Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int portNum = 6666;
+
+            try{
+                int userID = 0001;
+
+                Socket socket = new Socket("localhost", 6666);
+                BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter( new OutputStreamWriter(socket.getOutputStream()));
+                // send data to the server
+                // send command to the server
+                Scanner input = new Scanner(System.in);
+                System.out.println("login function"); //login
+
+                do {
+                    String command = input.nextLine();
+                    if (command.equals("log out"))
+                        break;
+                    out.println(command);
+                    out.flush();
+
+                    out.println("BYE");
+                    out.flush();
+                    // receive data from the server
+                    while (true) {
+                        String str = in.readLine();
+                        if (str == null) {
+                            break;
+                        } else {
+                            System.out.println(str);
+                        }
+                    }
+                } while (true);
+
+            }catch (Exception e) {
+
+            }
+
     }
     
     /**
